f22a68a5bf3a5ed340c841067d84c644cbfd3edd
ELW123 <ethanwan5@gmail.com>
2022-11-03 10:45:54 -0700

    write function needs fixing

--- user.py
+++ user.py
@@ -1,4 +1,4 @@
-## To be implemented: create user (ID, email, password) and add info to users.csv
+## create user (ID, email, password) and add info to users.csv
 ## Starting questionaire, record user answers into users.csv
 
 import csv
@@ -10,20 +10,27 @@ def user_creation(userInfo):
         print("That email is already tied into an account. \n")
         return None
     else: 
-        #userInfo["email"] = email
-        #userInfo["ID"] = user
         userInfo.update({"email": email1})
         user1 = input("Enter your username: \n")
         userInfo.update({"ID": user1})
         password1 = input("Enter your password: \n")
         userInfo.update({"password": password1})
-
+        write_to_userscsv(userInfo)
     # writes to user.csv (to be implemented later)
     # with open('user.csv', 'w') as file:
     #    writer = csv.writer(file)
     #    writer.writerow(userInfo)
 
+def write_to_userscsv(userInfo):
+    with open("users.csv", 'w') as csvfile:
+        #writer = csv.Dictwriter(csvfile, fieldnames = field_names)
+        writer = csv.Dictwriter(csvfile)
+        #writer.writeheader()
+        writer.writerow(userInfo)
+        csvfile.close()
+
+
+# initializes userInfo and creates userInfo
 userInfo = dict({"email": "Email", "ID": "User", "password": "Password"})
-userInfo = dict()
 for i in range(3):  
     user_creation(userInfo)
